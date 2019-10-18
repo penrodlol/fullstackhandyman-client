348dab581a8558d70d2fe0d32a3683f6883df713
penrodlol <penrod.christian7984@gmail.com>
2019-10-18 18:11:06 -0400

    Setting up some core module routing

--- fshandyman-client/src/app/app-routing.module.ts
+++ fshandyman-client/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'handy-cookies', loadChildren: './cookies/cookie.module#CookiesModule' },
+  { path: 'handy-https', loadChildren: './http/http.module#HttpModule' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- fshandyman-client/src/app/app.module.ts
+++ fshandyman-client/src/app/app.module.ts
@@ -4,15 +4,21 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MaterialModule } from 'src/external/style/material/material.module';
+import { CookiesModule } from './cookies/cookies.module';
+import { HttpModule } from './http/http.module';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    MaterialModule
+    MaterialModule,
+    CookiesModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
