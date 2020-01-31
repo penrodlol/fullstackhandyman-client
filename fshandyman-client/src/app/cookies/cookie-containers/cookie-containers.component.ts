295cd3037b2ee216852b0ab73bb51897e03cdb6b
penrodlol <penrod.christian7984@gmail.com>
2020-01-30 21:57:24 -0500

    Refactoring getting cookies maps containers to use behaviorsubject. Using api generated exception message to show within snackbar.

--- fshandyman-client/src/app/cookies/cookie-containers/cookie-containers.component.ts
+++ fshandyman-client/src/app/cookies/cookie-containers/cookie-containers.component.ts
@@ -4,58 +4,43 @@ import { CookieMapsContainers } from './models/cookie-maps-containers.model';
 import { DialogService } from '../../shared/dialog/dialog.service';
 import { CreateCookieContainerComponent } from './create-cookie-container/create-cookie-container.component';
 import { Observable } from 'rxjs';
-import { take, map } from 'rxjs/operators';
 import { SnackbarService } from 'src/app/shared/snackbar/snackbar.service';
 
 @Component({
-  // tslint:disable-next-line: component-selector
   selector: 'cookie-containers',
   templateUrl: './cookie-containers.component.html',
   styleUrls: ['./cookie-containers.component.scss']
 })
 export class CookieContainersComponent implements OnInit {
 
-  cookieMapsContainers: Observable<CookieMapsContainers[]>;
+  cookieMapsContainers: Observable<CookieMapsContainers[]> = this.cookieContainersService.currentCookieMapsContainers;
 
-  constructor(private cookieContainersService: CookieContainersService,
-    private dialogService: DialogService, private snackbarService: SnackbarService) { }
+  constructor(
+    private cookieContainersService: CookieContainersService,
+    private dialogService: DialogService,
+    private snackbarService: SnackbarService
+  ) { }
 
   ngOnInit() {
-    this.cookieMapsContainers = this.fetchCookieContainers();
-  }
-
-  fetchCookieContainers(): Observable<CookieMapsContainers[]> {
-    return this.cookieContainersService.cookieMapsContainers.pipe(take(1));
+    this.cookieContainersService.getCookieMapsContainers();
   }
 
   createCookieMapsContainer() {
     this.dialogService.showComponentDialog('Create Container', 'Create', CreateCookieContainerComponent, 'Cancel')
       .afterClosed().subscribe((result: any) => {
-        if (!result) { return; }
-
-        let recoredExists: boolean;
-
-        this.cookieMapsContainers
-          .pipe(map(containers => containers.filter(container => container.name === result.name)))
-          .subscribe(names => recoredExists = names.length > 0 ? true : false);
-
-        if (!recoredExists) {
-          this.cookieContainersService.createContainer(result.name).subscribe((cookieMapContainer: CookieMapsContainers) => {
-            this.snackbarService.openSnackbar({ text: `${cookieMapContainer.name} created!` });
-            this.appendCookieMapContainer(cookieMapContainer);
-          });
-        } else {
-          this.snackbarService.openSnackbar({ text: `${result.name} already exists.`, actionText:'Try again?' })
-            .subscribe(event => {
+        this.cookieContainersService.createContainer(result.name).subscribe(
+          (newCookieMapsContainer: CookieMapsContainers) => {
+            this.snackbarService.openSnackbar({ text: `${newCookieMapsContainer.name} created!` });
+            this.cookieContainersService.refreshCookieMapsContainers(newCookieMapsContainer);
+          },
+          ex => {
+            this.snackbarService.openSnackbar({ text: ex.error.reason, actionText: 'Try again?' }).subscribe(event => {
               if (event.dismissedByAction) {
                 this.createCookieMapsContainer();
               }
             });
-        }
+          }
+        );
       });
-  }
-
-  appendCookieMapContainer(cookieMapContainer: CookieMapsContainers) {
-    this.cookieMapsContainers.subscribe(containers => containers.push(cookieMapContainer));
   }
 }
--- fshandyman-client/src/app/cookies/cookie-containers/service/cookie-containers.service.ts
+++ fshandyman-client/src/app/cookies/cookie-containers/service/cookie-containers.service.ts
@@ -1,7 +1,7 @@
-import { Injectable, Inject } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable, Subject } from 'rxjs';
-import { shareReplay, switchMap, takeUntil} from 'rxjs/operators';
+import { Observable, BehaviorSubject, from, } from 'rxjs';
+import { shareReplay } from 'rxjs/operators';
 import { CookieMapsContainers } from '../models/cookie-maps-containers.model';
 import { UrlBuilderService } from 'src/app/shared/url/url-builder.service';
 
@@ -11,29 +11,30 @@ import { UrlBuilderService } from 'src/app/shared/url/url-builder.service';
 export class CookieContainersService {
   cookieUrl: string;
 
-  private currentCookieMapsContainers: Observable<CookieMapsContainers[]>;
+  private cookieMapsContainersSource: BehaviorSubject<CookieMapsContainers[]> = new BehaviorSubject([]);
+  currentCookieMapsContainers: Observable<CookieMapsContainers[]> = from(this.cookieMapsContainersSource);
 
   constructor(private urlBuilderService: UrlBuilderService, private http: HttpClient) {
     this.cookieUrl = this.urlBuilderService.buildUrl('cookie');
   }
-
-  get cookieMapsContainers() {
-    return !this.currentCookieMapsContainers
-    ? this.getCookieMapsContainers().pipe(shareReplay(1))
-    : this.currentCookieMapsContainers;
-  }
   
-  private getCookieMapsContainers(): Observable<CookieMapsContainers[]> {
+  getCookieMapsContainers(): void {
     const url = `${this.cookieUrl}/containers`;
-    return this.http.get<CookieMapsContainers[]>(url);
+    this.http.get<CookieMapsContainers[]>(url).subscribe(cookieMapsContainers => {
+      this.cookieMapsContainersSource.next(cookieMapsContainers);
+    });
+  }
+
+  refreshCookieMapsContainers(cookieMapsContainer: CookieMapsContainers) {
+    const currentCookieMapsContainers = this.cookieMapsContainersSource.getValue();
+    const newCookieMapsContainers = [ ...currentCookieMapsContainers, cookieMapsContainer];
+    this.cookieMapsContainersSource.next(newCookieMapsContainers);
   }
 
   createContainer(containerName: string): Observable<CookieMapsContainers> {
     const url = `${this.cookieUrl}/create/container`;
     return this.http.post<any>(url, null, {
-      headers: new HttpHeaders({
-        name: containerName
-      })
+      headers: new HttpHeaders({ name: containerName })
     });
   }
 }
