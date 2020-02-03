988c1f8da42813e9117b136c32a4e288b09d2039
penrodlol <penrod.christian7984@gmail.com>
2020-02-02 19:04:43 -0500

    Adding container tag to list

--- fshandyman-client/src/app/cookies/cookie-containers/models/cookie-maps-containers.model.ts
+++ fshandyman-client/src/app/cookies/cookie-containers/models/cookie-maps-containers.model.ts
@@ -1,10 +1,14 @@
 export class CookieMapsContainers {
     _containerNum: number;
     _name: string;
+    _tag: string;
 
     get containerNum(): number { return this._containerNum; }
     set containerNum(containerNum: number) { this._containerNum = containerNum; }
 
     get name(): string { return this._name; }
     set name(name: string) { this._name = name; }
+
+    get tag(): string { return this._tag; }
+    set tag(tag: string) { this._tag = tag; }
 }
